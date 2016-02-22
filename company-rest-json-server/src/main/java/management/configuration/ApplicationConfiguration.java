be9c54091eea582590a2c1f457559efb9336e471
Le Hong Nam <nam.lehong@axonactive.vn>
2016-02-22 15:10:06 +0700

    Refactor code and readme

--- company-rest-json-server/src/main/java/management/configuration/ApplicationConfiguration.java
+++ company-rest-json-server/src/main/java/management/configuration/ApplicationConfiguration.java
@@ -1,49 +1,14 @@
 package management.configuration;
 
-import management.security.LogoutHandler;
 import org.apache.catalina.Context;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.FilterRegistrationBean;
 import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class ApplicationConfiguration implements EmbeddedServletContainerCustomizer {
-
-    @Bean
-    LogoutSuccessHandler logoutSuccessHandler() {
-        return new LogoutHandler();
-    }
-
-    @Bean
-    public FilterRegistrationBean corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration corsConfig = new CorsConfiguration();
-
-        corsConfig.setAllowCredentials(true);
-        corsConfig.addAllowedOrigin("*");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedMethod("OPTIONS");
-        corsConfig.addAllowedMethod("HEAD");
-        corsConfig.addAllowedMethod("GET");
-        corsConfig.addAllowedMethod("PUT");
-        corsConfig.addAllowedMethod("POST");
-        corsConfig.addAllowedMethod("DELETE");
-        corsConfig.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
-        filterRegistrationBean.setOrder(0);
-        return filterRegistrationBean;
-    }
-
 
     @Override
     public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
--- company-rest-json-server/src/main/java/management/configuration/SecurityConfiguration.java
+++ company-rest-json-server/src/main/java/management/configuration/SecurityConfiguration.java
@@ -1,22 +1,51 @@
 package management.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    LogoutSuccessHandler logoutSuccessHandler;
-
+    /**
+     * Disable CSRF (Cross-Site Request Forgery Prevention)
+     */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
         http.csrf().disable();
+    }
+
+    /**
+     * Register bean to allow any CORS (Cross-Origin Resource Sharing).
+     */
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfig = new CorsConfiguration();
+
+        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedOrigin("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("OPTIONS");
+        corsConfig.addAllowedMethod("HEAD");
+        corsConfig.addAllowedMethod("GET");
+        corsConfig.addAllowedMethod("PUT");
+        corsConfig.addAllowedMethod("POST");
+        corsConfig.addAllowedMethod("DELETE");
+        corsConfig.addAllowedMethod("PATCH");
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        final FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new CorsFilter(source));
+        filterRegistrationBean.setOrder(0);
+        return filterRegistrationBean;
     }
 
     @Autowired
